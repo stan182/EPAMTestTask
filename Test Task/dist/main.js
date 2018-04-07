7377662a2cfc841c60aa575ca9f1b107ea51cec3
Stanislav Emshanov <stanislav.emshanov@gmail.com>
2018-04-07 16:21:48 +0400

    edit Test Task
    edit Test Task

--- Test Task/dist/main.js
+++ Test Task/dist/main.js
@@ -4,192 +4,191 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var URL = "https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture";
+(function () {
+    var URL = "https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture";
 
-var Users = function () {
-	function Users() {
-		_classCallCheck(this, Users);
+    var Users = function () {
+        function Users() {
+            _classCallCheck(this, Users);
 
-		this._users = [];
-	}
+            this._users = [];
+        }
 
-	_createClass(Users, [{
-		key: 'setUsers',
-		value: function setUsers(users) {
-			this._users = users;
-		}
-	}, {
-		key: 'getUsers',
-		value: function getUsers() {
-			return this._users;
-		}
-	}, {
-		key: 'findUserById',
-		value: function findUserById(userId) {
-			return this._users.filter(function (user) {
-				return user.id === userId;
-			})[0];
-		}
-	}, {
-		key: 'sortBy',
-		value: function sortBy(type, order) {
-			if (order === 'A-Z') {
-				this._users.sort(function (a, b) {
-					if (a[type] > b[type]) {
-						return 1;
-					}
-				});
-			} else if (order === 'Z-A') {
-				this._users.sort(function (a, b) {
-					if (a[type] < b[type]) {
-						return 1;
-					}
-				});
-			}
-		}
-	}]);
+        _createClass(Users, [{
+            key: 'setUsers',
+            value: function setUsers(users) {
+                this._users = users;
+            }
+        }, {
+            key: 'getUsers',
+            value: function getUsers() {
+                return this._users;
+            }
+        }, {
+            key: 'findUserById',
+            value: function findUserById(userId) {
+                return this._users.filter(function (user) {
+                    return user.id === userId;
+                })[0];
+            }
+        }, {
+            key: 'sortBy',
+            value: function sortBy(type, order) {
+                if (order === 'A-Z') {
+                    this._users.sort(function (a, b) {
+                        if (a[type] > b[type]) {
+                            return 1;
+                        }
+                    });
+                } else if (order === 'Z-A') {
+                    this._users.sort(function (a, b) {
+                        if (a[type] < b[type]) {
+                            return 1;
+                        }
+                    });
+                }
+            }
+        }]);
 
-	return Users;
-}();
+        return Users;
+    }();
 
-var App = function () {
-	function App() {
-		_classCallCheck(this, App);
-	}
+    var App = function () {
+        function App() {
+            _classCallCheck(this, App);
+        }
 
-	_createClass(App, [{
-		key: 'init',
-		value: function init() {
-			var _this = this;
+        _createClass(App, [{
+            key: 'init',
+            value: function init() {
+                var _this = this;
 
-			this.users = new Users();
+                this.users = new Users();
 
-			this._initViews();
-			this._getData().then(function (users) {
-				return _this._convertUsers(users);
-			}).then(this.users.setUsers.bind(this.users)).then(this._drawUsersList.bind(this));
-		}
-	}, {
-		key: '_initViews',
-		value: function _initViews() {
-			var _this2 = this;
+                this._initViews();
+                this._getData().then(function (users) {
+                    return _this._convertUsers(users);
+                }).then(this.users.setUsers.bind(this.users)).then(this._drawUsersList.bind(this));
+            }
+        }, {
+            key: '_initViews',
+            value: function _initViews() {
+                var _this2 = this;
 
-			//сортировка
-			this.sortNameEl = document.getElementById('sort-name');
-			this.sortOrderEl = document.getElementById('sort-order');
-			this.sortNameEl.addEventListener('change', this._onSortChanged.bind(this));
-			this.sortOrderEl.addEventListener('change', this._onSortChanged.bind(this));
+                //сортировка
+                this.sortNameEl = document.getElementById('sort-name');
+                this.sortOrderEl = document.getElementById('sort-order');
+                this.sortNameEl.addEventListener('change', this._onSortChanged.bind(this));
+                this.sortOrderEl.addEventListener('change', this._onSortChanged.bind(this));
+                this.body = document.getElementsByTagName("body")[0];
 
-			// попап
-			this.blockscreenEl = document.getElementById('blockscreen');
-			this.blockscreenEl.addEventListener('click', function (event) {
-				if (event.currentTarget === event.target) {
-					event.target.classList.remove('active');
-					_this2._scrollBodyOn();
-				}
-			});
+                // попап
+                this.blockscreenEl = document.getElementById('blockscreen');
+                this.blockscreenEl.addEventListener('click', function (event) {
+                    if (event.currentTarget === event.target) {
+                        event.target.classList.remove('active');
+                        _this2._scrollBodyOn();
+                    }
+                });
 
-			// контейнер
-			this.usersListContainer = document.getElementById('list');
-			this.usersListContainer.addEventListener('click', function (event) {
-				var target = event.target;
+                // контейнер
+                this.usersListContainer = document.getElementById('list');
+                this.usersListContainer.addEventListener('click', function (event) {
+                    var target = event.target;
 
-				// цикл двигается вверх от target к родителям до list
-				while (target != _this2.usersListContainer) {
-					if (target.id == 'user_card') {
-						// нашли элемент, который нас интересует!
-						_this2._onUserClick(target.dataset.id);
-						return;
-					}
-					target = target.parentNode;
-				}
-			});
-		}
-	}, {
-		key: '_getData',
-		value: function _getData() {
-			return fetch(URL).then(function (data) {
-				return data.json();
-			}).then(function (data) {
-				return data.results;
-			}).catch(function (error) {
-				alert(error);
-			});
-		}
-	}, {
-		key: '_getSelectedValue',
-		value: function _getSelectedValue(select) {
-			return select.options[select.selectedIndex].value;
-		}
-	}, {
-		key: '_convertUsers',
-		value: function _convertUsers(users) {
-			return users.map(function (user, index) {
-				return {
-					id: index,
-					avatarMedium: user.picture.medium,
-					avatarLarge: user.picture.large,
-					title: user.name.title,
-					firstName: user.name.first,
-					lastName: user.name.last,
-					street: user.location.street,
-					city: user.location.city,
-					state: user.location.state,
-					postcode: user.location.postcode,
-					email: user.email,
-					phone: user.phone
-				};
-			});
-		}
-	}, {
-		key: '_drawUsersList',
-		value: function _drawUsersList() {
-			var _this3 = this;
+                    while (target != _this2.usersListContainer) {
+                        if (target.id == 'user_card') {
+                            _this2._onUserClick(target.dataset.id);
+                            return;
+                        }
+                        target = target.parentNode;
+                    }
+                });
+            }
+        }, {
+            key: '_getData',
+            value: function _getData() {
+                return fetch(URL).then(function (data) {
+                    return data.json();
+                }).then(function (data) {
+                    return data.results;
+                }).catch(function (error) {
+                    alert(error);
+                });
+            }
+        }, {
+            key: '_getSelectedValue',
+            value: function _getSelectedValue(select) {
+                return select.options[select.selectedIndex].value;
+            }
+        }, {
+            key: '_convertUsers',
+            value: function _convertUsers(users) {
+                return users.map(function (user, index) {
+                    return {
+                        id: index,
+                        avatarMedium: user.picture.medium,
+                        avatarLarge: user.picture.large,
+                        title: user.name.title,
+                        firstName: user.name.first,
+                        lastName: user.name.last,
+                        street: user.location.street,
+                        city: user.location.city,
+                        state: user.location.state,
+                        postcode: user.location.postcode,
+                        email: user.email,
+                        phone: user.phone
+                    };
+                });
+            }
+        }, {
+            key: '_drawUsersList',
+            value: function _drawUsersList() {
+                var _this3 = this;
 
-			var result = '';
+                var result = '';
 
-			this.users.getUsers().forEach(function (user) {
-				result += _this3._getUsersItemTemplate(user);
-			});
+                this.users.getUsers().forEach(function (user) {
+                    result += _this3._getUsersItemTemplate(user);
+                });
 
-			this.usersListContainer.innerHTML = result;
-		}
-	}, {
-		key: '_getUsersItemTemplate',
-		value: function _getUsersItemTemplate(user) {
-			return '<div id="user_card" data-id="' + user.id + '" class="shadow">\n\t\t\t\t<img src="' + user.avatarMedium + '"/></td>\n\t\t\t\t<span>' + user.title + '. ' + user.firstName + ' ' + user.lastName + '</span>\n\t\t\t</div>';
-		}
-	}, {
-		key: '_getBlockscreenTemplate',
-		value: function _getBlockscreenTemplate(user) {
-			return '<div class="shadow" id="info">\n\t\t\t<h1 id="username">' + user.title + '. ' + user.firstName + ' ' + user.lastName + '</h1>\n\t\t\t<img id="photo" src="' + user.avatarLarge + '">\n\t\t\t<div>\n\t\t\t\t<span id="address"><b>Address:</b> ' + user.postcode + ', ' + user.state + ', ' + user.city + ', ' + user.street + '</span><br>\n\t\t\t\t<span id="conacts"><b>Contacts:</b> ' + user.phone + ', ' + user.email + '</span>\n\t\t\t</div>\n\t\t</div>';
-		}
-	}, {
-		key: '_onUserClick',
-		value: function _onUserClick(userId) {
-			var clickedUser = this.users.findUserById(+userId);
-			this.blockscreenEl.innerHTML = this._getBlockscreenTemplate(clickedUser);
-			this.blockscreenEl.classList.add('active');
-			var body = document.getElementsByTagName("body")[0];
-			body.setAttribute("style", "overflow: hidden");
-		}
-	}, {
-		key: '_onSortChanged',
-		value: function _onSortChanged() {
-			this.users.sortBy(this._getSelectedValue(this.sortNameEl), this._getSelectedValue(this.sortOrderEl));
-			this._drawUsersList();
-		}
-	}, {
-		key: '_scrollBodyOn',
-		value: function _scrollBodyOn() {
-			var body = document.getElementsByTagName("body")[0];
-			body.removeAttribute("style", "overflow: hidden");
-		}
-	}]);
+                this.usersListContainer.innerHTML = result;
+            }
+        }, {
+            key: '_getUsersItemTemplate',
+            value: function _getUsersItemTemplate(user) {
+                return '<div id="user_card" data-id="' + user.id + '" class="shadow">\n\t\t\t\t<img src="' + user.avatarMedium + '"/></td>\n\t\t\t\t<span>' + user.title + '. ' + user.firstName + ' ' + user.lastName + '</span>\n\t\t\t</div>';
+            }
+        }, {
+            key: '_getBlockscreenTemplate',
+            value: function _getBlockscreenTemplate(user) {
+                return '<div class="shadow" id="info">\n\t\t\t<h1 id="username">' + user.title + '. ' + user.firstName + ' ' + user.lastName + '</h1>\n\t\t\t<img id="photo" src="' + user.avatarLarge + '">\n\t\t\t<div>\n\t\t\t\t<span id="address"><b>Address:</b> ' + user.postcode + ', ' + user.state + ', ' + user.city + ', ' + user.street + '</span><br>\n\t\t\t\t<span id="conacts"><b>Contacts:</b> ' + user.phone + ', ' + user.email + '</span>\n\t\t\t</div>\n\t\t</div>';
+            }
+        }, {
+            key: '_onUserClick',
+            value: function _onUserClick(userId) {
+                var clickedUser = this.users.findUserById(+userId);
+                this.blockscreenEl.innerHTML = this._getBlockscreenTemplate(clickedUser);
+                this.blockscreenEl.classList.add('active');
+                this.body.setAttribute("style", "overflow: hidden");
+            }
+        }, {
+            key: '_onSortChanged',
+            value: function _onSortChanged() {
+                this.users.sortBy(this._getSelectedValue(this.sortNameEl), this._getSelectedValue(this.sortOrderEl));
+                this._drawUsersList();
+            }
+        }, {
+            key: '_scrollBodyOn',
+            value: function _scrollBodyOn() {
+                this.body.removeAttribute("style", "overflow: hidden");
+            }
+        }]);
 
-	return App;
-}();
+        return App;
+    }();
 
-var app = new App();
+    var app = new App();
 
-window.addEventListener('load', app.init.bind(app));
+    window.addEventListener('load', app.init.bind(app));
+})();
--- Test Task/src/main.js
+++ Test Task/src/main.js
@@ -1,133 +1,132 @@
-const URL = "https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture";
+(function () {
+    const URL = "https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture";
 
-class Users {
-	constructor() {
-		this._users = [];
-	}
+    class Users {
+        constructor() {
+            this._users = [];
+        }
 
-	setUsers(users) {
-		this._users = users;
-	}
+        setUsers(users) {
+            this._users = users;
+        }
 
-	getUsers() {
-		return this._users
-	}
+        getUsers() {
+            return this._users
+        }
 
-	findUserById(userId) {
-		return this._users.filter((user) => user.id === userId)[0];
-	}
+        findUserById(userId) {
+            return this._users.filter((user) => user.id === userId)[0];
+        }
 
-	sortBy(type, order) {
-		if (order === 'A-Z') {
-			this._users.sort((a,b) => {
-				if (a[type] > b[type]) {
-					return 1;
-				}
-			});
-		} else if (order === 'Z-A') {
-			this._users.sort((a,b) => {
-				if (a[type] < b[type]) {
-					return 1;
-				}
-			});
-		}
-	}
-}
+        sortBy(type, order) {
+            if (order === 'A-Z') {
+                this._users.sort((a,b) => {
+                    if (a[type] > b[type]) {
+                        return 1;
+                    }
+                });
+            } else if (order === 'Z-A') {
+                this._users.sort((a,b) => {
+                    if (a[type] < b[type]) {
+                        return 1;
+                    }
+                });
+            }
+        }
+    }
 
-class App {
-	init() {
-		this.users = new Users();
+    class App {
+        init() {
+            this.users = new Users();
 
-		this._initViews();
-		this._getData()
-			.then(users => this._convertUsers(users))
-			.then(this.users.setUsers.bind(this.users))
-			.then(this._drawUsersList.bind(this));
-	}
+            this._initViews();
+            this._getData()
+                .then(users => this._convertUsers(users))
+                .then(this.users.setUsers.bind(this.users))
+                .then(this._drawUsersList.bind(this));
+        }
 
-	_initViews() {
-		//сортировка
-		this.sortNameEl = document.getElementById('sort-name');
-		this.sortOrderEl = document.getElementById('sort-order');
-		this.sortNameEl.addEventListener('change', this._onSortChanged.bind(this));
-		this.sortOrderEl.addEventListener('change', this._onSortChanged.bind(this));
+        _initViews() {
+            //сортировка
+            this.sortNameEl = document.getElementById('sort-name');
+            this.sortOrderEl = document.getElementById('sort-order');
+            this.sortNameEl.addEventListener('change', this._onSortChanged.bind(this));
+            this.sortOrderEl.addEventListener('change', this._onSortChanged.bind(this));
+            this.body = document.getElementsByTagName("body")[0];
 
+            // попап
+            this.blockscreenEl = document.getElementById('blockscreen');
+            this.blockscreenEl.addEventListener('click', (event) => {
+                if (event.currentTarget === event.target) {
+                    event.target.classList.remove('active');
+                    this._scrollBodyOn();
+                }
+            });
 
-		// попап
-		this.blockscreenEl = document.getElementById('blockscreen');
-		this.blockscreenEl.addEventListener('click', (event) => {
-			if (event.currentTarget === event.target) {
-				event.target.classList.remove('active');
-                this._scrollBodyOn();
-			}
-		});
+            // контейнер
+            this.usersListContainer = document.getElementById('list');
+            this.usersListContainer.addEventListener('click', (event) => {
+                var target = event.target;
 
-		// контейнер
-		this.usersListContainer = document.getElementById('list');
-		this.usersListContainer.addEventListener('click', (event) => {
-			var target = event.target;
+                while (target != this.usersListContainer) {
+                    if (target.id == 'user_card') {
+                        this._onUserClick(target.dataset.id);
+                        return;
+                    }
+                    target = target.parentNode;
+                }
+            });
+        }
 
-			// цикл двигается вверх от target к родителям до list
-			while (target != this.usersListContainer) {
-				if (target.id == 'user_card') {
-					// нашли элемент, который нас интересует!
-					this._onUserClick(target.dataset.id);
-					return;
-				}
-				target = target.parentNode;
-			}
-		});
-	}
+        _getData() {
+            return fetch(URL)
+                .then(data => data.json())
+                .then((data) => data.results)
+                .catch((error) => {alert(error)});
+        }
 
-	_getData() {
-		return fetch(URL)
-			.then(data => data.json())
-			.then((data) => data.results)
-			.catch((error) => {alert(error)});
-	}
+        _getSelectedValue(select) {
+            return select.options[select.selectedIndex].value;
+        }
 
-	_getSelectedValue(select) {
-		return select.options[select.selectedIndex].value;
-	}
+        _convertUsers(users) {
+            return users.map((user, index) => {
+                return {
+                    id: index,
+                    avatarMedium: user.picture.medium,
+                    avatarLarge: user.picture.large,
+                    title: user.name.title,
+                    firstName: user.name.first,
+                    lastName: user.name.last,
+                    street: user.location.street,
+                    city: user.location.city,
+                    state: user.location.state,
+                    postcode: user.location.postcode,
+                    email: user.email,
+                    phone: user.phone,
+                };
+            })
+        }
 
-	_convertUsers(users) {
-		return users.map((user, index) => {
-			return {
-				id: index,
-				avatarMedium: user.picture.medium,
-				avatarLarge: user.picture.large,
-				title: user.name.title,
-				firstName: user.name.first,
-				lastName: user.name.last,
-				street: user.location.street,
-				city: user.location.city,
-				state: user.location.state,
-				postcode: user.location.postcode,
-				email: user.email,
-				phone: user.phone,
-			};
-		})
-	}
+        _drawUsersList() {
+            let result = '';
 
-	_drawUsersList() {
-		let result = '';
+            this.users.getUsers().forEach((user) => {
+                result += this._getUsersItemTemplate(user);
+            });
 
-		this.users.getUsers().forEach((user) => {
-			result += this._getUsersItemTemplate(user);
-		});
+            this.usersListContainer.innerHTML = result;
+        }
 
-		this.usersListContainer.innerHTML = result;
-	}
-
-	_getUsersItemTemplate(user) {
-		return `<div id="user_card" data-id="${user.id}" class="shadow">
+        _getUsersItemTemplate(user) {
+            return `<div id="user_card" data-id="${user.id}" class="shadow">
 				<img src="${user.avatarMedium}"/></td>
 				<span>${user.title}. ${user.firstName} ${user.lastName}</span>
 			</div>`;
-	}
+        }
 
-	_getBlockscreenTemplate(user) {
-		return `<div class="shadow" id="info">
+        _getBlockscreenTemplate(user) {
+            return `<div class="shadow" id="info">
 			<h1 id="username">${user.title}. ${user.firstName} ${user.lastName}</h1>
 			<img id="photo" src="${user.avatarLarge}">
 			<div>
@@ -135,27 +134,26 @@ class App {
 				<span id="conacts"><b>Contacts:</b> ${user.phone}, ${user.email}</span>
 			</div>
 		</div>`;
-	}
+        }
 
-	_onUserClick(userId) {
-		const clickedUser = this.users.findUserById(+userId);
-		this.blockscreenEl.innerHTML = this._getBlockscreenTemplate(clickedUser);
-		this.blockscreenEl.classList.add('active');
-		const body = document.getElementsByTagName("body")[0];
-		body.setAttribute("style", "overflow: hidden");
-	}
+        _onUserClick(userId) {
+            const clickedUser = this.users.findUserById(+userId);
+            this.blockscreenEl.innerHTML = this._getBlockscreenTemplate(clickedUser);
+            this.blockscreenEl.classList.add('active');
+            this.body.setAttribute("style", "overflow: hidden");
+        }
 
-	_onSortChanged() {
-		this.users.sortBy(this._getSelectedValue(this.sortNameEl), this._getSelectedValue(this.sortOrderEl));
-		this._drawUsersList();
-	}
+        _onSortChanged() {
+            this.users.sortBy(this._getSelectedValue(this.sortNameEl), this._getSelectedValue(this.sortOrderEl));
+            this._drawUsersList();
+        }
 
-	_scrollBodyOn() {
-        const body = document.getElementsByTagName("body")[0];
-        body.removeAttribute("style", "overflow: hidden");
-	}
-}
+        _scrollBodyOn() {
+            this.body.removeAttribute("style", "overflow: hidden");
+        }
+    }
 
-const app = new App();
+    const app = new App();
 
-window.addEventListener('load', app.init.bind(app));
+    window.addEventListener('load', app.init.bind(app));
+})()
